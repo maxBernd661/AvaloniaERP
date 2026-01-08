7cd8ecb4e090ce0d78f95f9e22daaa0512b9c6e0
Max Bernd <macbe@hotmail.de>
2026-01-08 10:10:33 +0100

    initial entity setup

---
+++ AvaloniaERP.Core/Entity/Product.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AvaloniaERP.Core.Entity
+{
+    public class Product : PersistentBase
+    {
+        public string Name { get; set; }
+
+        public decimal PricePerUnit { get; set; }
+
+        public double Weight { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> b)
+        {
+            b.ToTable(nameof(Product));
+            b.HasKey(p => p.Id);
+
+            b.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
