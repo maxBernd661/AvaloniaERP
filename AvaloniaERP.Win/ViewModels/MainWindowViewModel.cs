a2f89a9fbf10c64d6629bf26134c314543ad44ea
Max Bernd <macbe@hotmail.de>
2026-01-08 08:54:30 +0100

    initial commit

---
+++ AvaloniaERP.Win/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaERP.Win.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
