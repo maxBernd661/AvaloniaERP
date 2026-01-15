2cac481008dae3035db81146e39493ed09dfa5c7
Reminder <22225953+IReminder@users.noreply.github.com>
2026-01-15 09:01:07 +0100

    Create dashboard navigation shell

---
+++ AvaloniaERP.Win/ViewModels/DashboardHomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace AvaloniaERP.Win.ViewModels
+{
+    public sealed class DashboardHomeViewModel : ViewModelBase
+    {
+        public string Title { get; } = "Dashboard";
+        public string Summary { get; } = "Choose a view from the navigation to get started.";
+    }
+}
--- AvaloniaERP.Win/ViewModels/MainWindowViewModel.cs
+++ AvaloniaERP.Win/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,44 @@
-﻿namespace AvaloniaERP.Win.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace AvaloniaERP.Win.ViewModels
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    public sealed class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        private NavigationItem? selectedNavigationItem;
+        private ViewModelBase? currentViewModel;
+
+        public MainWindowViewModel(ProductListViewModel productListViewModel)
+        {
+            NavigationItems = new ObservableCollection<NavigationItem>
+            {
+                new("Dashboard", new DashboardHomeViewModel()),
+                new("Products", productListViewModel)
+            };
+
+            SelectedNavigationItem = NavigationItems.First();
+        }
+
+        public ObservableCollection<NavigationItem> NavigationItems { get; }
+
+        public NavigationItem? SelectedNavigationItem
+        {
+            get => selectedNavigationItem;
+            set
+            {
+                if (SetProperty(ref selectedNavigationItem, value))
+                {
+                    CurrentViewModel = selectedNavigationItem?.ViewModel;
+                }
+            }
+        }
+
+        public ViewModelBase? CurrentViewModel
+        {
+            get => currentViewModel;
+            private set => SetProperty(ref currentViewModel, value);
+        }
     }
+
+    public sealed record NavigationItem(string Title, ViewModelBase ViewModel);
 }
